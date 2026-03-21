3a56fd993c93fbebc0a29acc5e7cbd2c33e81282
Yashashri Sawant <sawantrajan533@gmail.com>
2026-03-21 21:42:12 +0530

    Delete lms-system directory

--- lms-system/backend/routes/courseroutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const courseController = require('../controllers/coursecontroller');
-
-router.post('/create', courseController.createCourse);
-router.get('/', courseController.getCourses);
-
-module.exports = router;
